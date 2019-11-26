a475a5c2aff14fed16c1a11bc074fd0b9727e151
Alberto Hernandez <alberto.hernandez@inovatika.cz>
2019-11-26 13:42:28 +0100

    popover

--- web/src/main/ng-client/src/app/components/result-item/result-item.component.ts
+++ web/src/main/ng-client/src/app/components/result-item/result-item.component.ts
@@ -1,16 +1,85 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, ViewContainerRef, TemplateRef } from '@angular/core';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-result-item',
   templateUrl: './result-item.component.html',
   styleUrls: ['./result-item.component.scss']
 })
-export class ResultItemComponent implements OnInit {
+export class ResultItemComponent implements OnInit, OnDestroy {
+  private overlayRef: OverlayRef;
   @Input() doc;
 
-  constructor() { }
+  public tooltip: {
+    field: string,
+    text: string
+  } = {
+      field: '',
+      text: ''
+    };
+
+  constructor(
+    private overlay: Overlay,
+    private viewContainerRef: ViewContainerRef
+  ) { }
 
   ngOnInit() {
   }
+
+  ngOnDestroy() {
+    this.closePop();
+  }
+
+  hasDifferences(field: string): boolean {
+    const arr: Array<string> = this.doc[field];
+    return !arr.every(v => v === arr[0]);
+  }
+
+  openPop(field: string, relative: any, template: TemplateRef<any>) {
+    const arr: Array<string> = this.doc[field];
+    this.tooltip = {
+      field: field,
+      text: arr.join('<br/>')
+    };
+    console.log(this.tooltip);
+    this.closeInfoOverlay();
+    setTimeout(() => {
+      this.openInfoOverlay(relative, template);
+    }, 200);
+  }
+
+  closePop() {
+    this.closeInfoOverlay();
+  }
+
+  openInfoOverlay(relative: any, template: TemplateRef<any>) {
+    this.closeInfoOverlay();
+
+    this.overlayRef = this.overlay.create({
+      positionStrategy: this.overlay.position().flexibleConnectedTo(relative._elementRef).withPositions([{
+        overlayX: 'end',
+        overlayY: 'top',
+        originX: 'center',
+        originY: 'bottom'
+      }]).withPush().withViewportMargin(30).withDefaultOffsetX(37).withDefaultOffsetY(20),
+      scrollStrategy: this.overlay.scrollStrategies.close(),
+      hasBackdrop: true,
+      backdropClass: 'popover-backdrop'
+    });
+    this.overlayRef.backdropClick().subscribe(() => this.closeInfoOverlay());
+
+    const portal = new TemplatePortal(template, this.viewContainerRef);
+    this.overlayRef.attach(portal);
+  }
+
+  closeInfoOverlay() {
+    if (this.overlayRef) {
+      this.overlayRef.detach();
+      this.overlayRef.dispose();
+      this.overlayRef = null;
+    }
+  }
+
 
 }
